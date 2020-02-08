467b8dae15bad8fde036a867e9408bfd35a28721
amar <amar.jsr18@gmail.com>
2020-02-08 21:20:21 +0530

    modify app component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <p> HelloWorld!!</p>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-content">
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
