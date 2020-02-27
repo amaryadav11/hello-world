bdaf1705b242b5fbbf3a6a417e1c3d818c803695
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 12:46:37 +0000

    Deploy amaryadav11/hello-world to github.com/amaryadav11/hello-world.git:gh-pages

---
+++ precache-manifest.890e3d35c7d5d948879651facccae3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9137ac8aad4d47d2ba17e65c0df767e9",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "eb2ac4ec417a8eda656b",
+    "url": "/hello-world/static/css/main.429fb3fa.chunk.css"
+  },
+  {
+    "revision": "f25c885a782ba9de3a67",
+    "url": "/hello-world/static/js/2.0cc654b1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hello-world/static/js/2.0cc654b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2ac4ec417a8eda656b",
+    "url": "/hello-world/static/js/main.042fbded.chunk.js"
+  },
+  {
+    "revision": "3519b3f14ed7948ab849",
+    "url": "/hello-world/static/js/runtime-main.a0f97cd6.js"
+  }
+]);
